db881df84ca506e0fd8b8fea267ba6f2097cff2a
Andy Agus <redagus@gmail.com>
2022-11-13 11:54:49 -0500

    implemented color manager subscribe to key

--- Assets/Scripts/AR Keyboard/AudioManager.cs
+++ Assets/Scripts/AR Keyboard/AudioManager.cs
@@ -3,14 +3,16 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using AR_Keyboard;
+using Enums;
 using Interfaces;
-using UnityEditor;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour, IObserver
 {
     private AudioSource _audioSource;
     private List<ARPrimaryKey> _primaryKeys;
+    [SerializeField] private AudioClip bellClip;
+    
     
     private void Awake()
     {
@@ -29,6 +31,13 @@ public class AudioManager : MonoBehaviour, IObserver
     public void OnNotify(object entity)
     {
         Shortcut shortcut = (Shortcut)entity;
+        switch (shortcut.eShortcut)
+        {
+            case EShortcuts.CUT_SHORTCUT:
+                _audioSource.clip = bellClip;
+                _audioSource.Play();
+                break;
+        }
         // entity.
     }
 }
--- Assets/Scripts/Effects/KeyColorManager.cs
+++ Assets/Scripts/Effects/KeyColorManager.cs
@@ -1,14 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using AR_Keyboard;
+using Interfaces;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Effects
 {
-    public class KeyColorManager : MonoBehaviour
+    public class KeyColorManager : MonoBehaviour, IObserver
     {
+        private Color originalShortcutColor;
+        
+        private List<ARPrimaryKey> _primaryKeys;
+        //could switch to other………
+
+        private void Awake()
+        {
+            _primaryKeys = GameObject.Find("AR Keyboard").GetComponentsInChildren<ARPrimaryKey>().ToList();
+        }
+
+        private void Start()
+        {
+            foreach (var primaryKey in _primaryKeys)
+            {
+                primaryKey.onPrimaryKeyHit.AddObserver(this);
+            }
+        }
+
+        public void OnNotify(object entity)
+        {
+            Shortcut shortcut = (Shortcut)entity;
+            originalShortcutColor = shortcut.GetComponentInChildren<Image>().color;
+            shortcut.GetComponentInChildren<Image>().color = Color.green;
+            StartCoroutine(RevertBackColorTimer(shortcut));
+        }
+
+        private IEnumerator RevertBackColorTimer(Shortcut shortcut)
+        {
+            yield return new WaitForSeconds(0.4f);
+            shortcut.GetComponentInChildren<Image>().color = originalShortcutColor;
+        }
+
         public static void ChangeKeyColor(ARKey key, Color color)
         {
             var keyMaterial= key.GetComponent<MeshRenderer>().material;
             keyMaterial.color = color;
         }
+        
     }
 }
