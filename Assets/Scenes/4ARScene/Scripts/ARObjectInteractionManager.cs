f1c57bbc1d2c567ddd738b0508c06ffafe679e00
Andy Agus <redagus@gmail.com>
2022-11-18 12:51:25 -0500

    set up initial keyboard game obejct

--- Assets/Scenes/4ARScene/Scripts/ARObjectInteractionManager.cs
+++ Assets/Scenes/4ARScene/Scripts/ARObjectInteractionManager.cs
@@ -1,18 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.AR;
 
 public class ARObjectInteractionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ARPlaneManager planeManager;
+    [SerializeField] private ARPlacementInteractable placementInteractable;    
+    [SerializeField] private ARGestureInteractor gestureInteractor;
+    
+    private bool _objectPlaced;
+
+    
+    // ReSharper disable once InconsistentNaming
+    [SerializeField] private GameObject _ARKeyboard;
+
+    private void Awake()
     {
-        
+        planeManager.planesChanged += OnPlanesChanged;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPlanesChanged(ARPlanesChangedEventArgs obj)
     {
-        
+        if (_objectPlaced)
+        {
+            foreach (var trackable in planeManager.trackables)
+            {
+                trackable.gameObject.SetActive(false);
+            }
+        }
     }
+
+    public void PlacedObject(ARObjectPlacementEventArgs eventArgs)
+    {
+        placementInteractable.enabled = false;
+        _objectPlaced = true;
+        
+        foreach (var trackable in planeManager.trackables)
+        {
+            trackable.gameObject.SetActive(false);
+        }
+    }
+
+    public void ObjectExited(SelectExitEventArgs eventArgs)
+    {
+        var interactableObj = eventArgs.interactableObject;
+        var objTransform = interactableObj.transform;
+        
+        gestureInteractor.enabled = false;
+        objTransform.gameObject.SetActive(false);
+        
+        //instantiate new game obejct at the position of interactable object
+
+        var keyboard = Instantiate(_ARKeyboard, objTransform.position, objTransform.rotation, objTransform.parent);
+    }
+    
 }
