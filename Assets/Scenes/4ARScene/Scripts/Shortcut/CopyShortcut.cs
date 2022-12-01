59cb3886bbfb63489fdb696288f06c43891bd22b
Andy Agus <redagus@gmail.com>
2022-12-01 12:20:17 -0500

    added copy and paste shortcut

--- Assets/Scenes/4ARScene/Scripts/Shortcut/CopyShortcut.cs
+++ Assets/Scenes/4ARScene/Scripts/Shortcut/CopyShortcut.cs
@@ -1,16 +1,66 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using AR_Keyboard;
 using AR_Keyboard.State;
+using DG.Tweening;
 using Enums;
+using TMPro;
 using UnityEngine;
 
 public class CopyShortcut : Shortcut
 {
+    public Transform stamp;
+
+    // private Vector3 _originalPosition;
+
+    public Transform executePosition;
     
+    private Sequence _setGraphicSequence;
+    private Sequence _executeSequence;
     
+    public float localYMove = 0.007f;
+
+    public bool graphicCopied;
+
+    public Action onGraphicCopied;
+    
+    public override void SetGraphics(ARPrimaryKey key)
+    {
+
+        // _originalPosition = transform.position;
+        _setGraphicSequence = DOTween.Sequence();
+        _setGraphicSequence.Append(stamp.DOLocalMoveY(localYMove, 1.2f));
+    }
+
     public override void Execute(EKeyState keyState, ARPrimaryKey key)
     {
-        Debug.Log("Copy Shortcut Called");
+        _executeSequence = DOTween.Sequence();
+        var newPos = new Vector3(0.0001f, .0025f, -0.002f);
+        _executeSequence.Append(stamp.DOLocalMove(newPos, 1f));
+        
+        var keyText = key.GetComponentInChildren<TextMeshProUGUI>();
+        _executeSequence.Insert(0, keyText.DOFade(0.12f , 1.13f));
+
+        var moveToVKeyPos = new Vector3(0.0187f, 0.0097f, -0.002f);
+        _executeSequence.Append(stamp.DOLocalMove(moveToVKeyPos, 0.896453f));
+
+        graphicCopied = true;
+        onGraphicCopied();
+    }
+
+    public override void StopSequence()
+    {
+        if (_setGraphicSequence != null)
+        {
+            _setGraphicSequence.Pause();
+            _setGraphicSequence.Kill();
+        }
+
+        if (_executeSequence != null)
+        {
+            _executeSequence.Pause();
+            _executeSequence.Kill();
+        }
     }
 }
--- Assets/Scenes/4ARScene/Scripts/Shortcut/PasteShortcut.cs
+++ Assets/Scenes/4ARScene/Scripts/Shortcut/PasteShortcut.cs
@@ -1,13 +1,74 @@
+using System;
+using DG.Tweening;
 using Enums;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 namespace AR_Keyboard.Shortcuts.Scripts
 {
    public class PasteShortcut : Shortcut
    {
+
+      private Sequence _executeSequence;
+      private TextMeshProUGUI _mainKeyText
+         ;
+      public TextMeshProUGUI cLetterText;
+
+      private CopyShortcut _copyShortcut;
+      
+      public override void Awake()
+      {
+         _mainKeyText = GameObject.Find("V").GetComponentInChildren<TextMeshProUGUI>();
+
+         _copyShortcut = GameObject.Find("C").GetComponentInChildren<CopyShortcut>();
+         base.Awake();
+      }
+
+      private void Start()
+      {
+         _copyShortcut.onGraphicCopied += OnGraphicCopied;
+      }
+
+      private void OnGraphicCopied()
+      {
+
+         _mainKeyText.DOFade(1, 2.7f);
+         // var keyText = GetComponentInParent<TextMeshProUGUI>();
+         // keyText.DOFade(0f, 1f);
+      }
+
       public override void Execute(EKeyState keyState, ARPrimaryKey key)
       {
-         Debug.Log("Paste shortcut called");
+
+
+         if (_copyShortcut.graphicCopied)
+         {
+            _executeSequence = DOTween.Sequence();
+
+            var newPos = new Vector3(0.0173f, 0.0026f, -.001f);
+            var postPos = new Vector3(0.0187f, 0.0097f, -0.002f);
+            
+            _executeSequence.Append(_copyShortcut.stamp.DOLocalMove(newPos, 0.1f).SetEase(Ease.Linear))
+               .Append(cLetterText.DOFade(1, 0f))
+               .Append(_copyShortcut.stamp.DOLocalMove(postPos, 0.2f).SetEase(Ease.InSine));
+         }
+
+      }
+
+      
+      
+      
+      public override void StopSequence()
+      {
+         base.StopSequence();
+      }
+
+      public override void SetGraphics(ARPrimaryKey key)
+      {
+         _mainKeyText.DOFade(0.1f, 2f);
+
+         base.SetGraphics(key);
       }
    }
 }
