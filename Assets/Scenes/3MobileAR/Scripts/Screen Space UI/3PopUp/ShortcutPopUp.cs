838dab643edc5ababd74db35958363dcb9864a11
Andy Agus <redagus@gmail.com>
2023-01-09 13:34:01 -0500

    updated shortcut pop up scripts

--- Assets/Scenes/3MobileAR/Scripts/Screen Space UI/3PopUp/ShortcutPopUp.cs
+++ Assets/Scenes/3MobileAR/Scripts/Screen Space UI/3PopUp/ShortcutPopUp.cs
@@ -9,7 +9,8 @@ namespace Scenes._3MobileAR.Scripts.Screen_Space_UI._3PopUp
     /// </summary>
     public class ShortcutPopUp : MonoBehaviour
     {
-        //TODO: using shortcutListItem for popUp (same class as ListItem prefab)...switch its own class 
+        //TODO: using shortcutListItem for popUp (same class as ListItem prefab)...switch its own class
+        //Using ShortcutListItem to reference the UIPopUp prefab in the scene and access its text/image elements
         private ShortcutListItem _shortcutListItem;
         public ShortcutPopUpState.EShortcutPopUp popUpState;
         private ShortcutPopUpState _popUpState;
@@ -23,12 +24,13 @@ namespace Scenes._3MobileAR.Scripts.Screen_Space_UI._3PopUp
         }
     
         /// <summary>
-        /// When List Item is pressed set PopUp to active with active shortcut name and image
+        /// When shortcut in list is pressed (subscribed in awake) set PopUp to active with active shortcut name and image
         /// </summary>
         private void OnListItemClicked(Shortcut shortcut)
         {
             popUpState = ShortcutPopUpState.EShortcutPopUp.ACTIVE;
             _popUpState.SetPopUpState(popUpState);
+            //TODO: Refactor - setting shortcut list item image and text here, for consistency move to state machine.
             _shortcutListItem.shortcutName.text = shortcut.shortcutName;
             _shortcutListItem.shortcutImage.sprite = shortcut.shortcutSprite;
         }
