1bd61777eaef8278dc815cf907feadb4a539e209
Andy Agus <redagus@gmail.com>
2023-01-10 11:34:36 -0500

    added comments to desktop scene manager
    ad

--- Assets/Scenes/1Desktop/Managers/DesktopSceneManager.cs
+++ Assets/Scenes/1Desktop/Managers/DesktopSceneManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace Scenes._1Desktop
+namespace Scenes._1Desktop.Managers
 {
+    /// <summary>
+    /// Additively loads Normcore Common Scene on top of desktop scene.
+    /// </summary>
     public class DesktopSceneManager : MonoBehaviour
     {
         private void Awake()
